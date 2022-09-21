be8982d164941b16f89885a73ca72d9d581d5c95
Екатерина <katushkars@mail.ru>
2022-09-21 22:19:22 +0500

    первая часть

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //задание 1
+        System.out.println("Задание 1");
+        String firstName ="Семён ";
+        String middleName ="Семёнович ";
+        String lastName ="Иванов ";
+        String fullName= lastName+firstName+middleName;
+        fullName = fullName.replace('ё','e');
+        System.out.println("ФИО сотрудника - "+fullName);
+        String upperFullName=fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета- "+upperFullName);
+
+
     }
 }
